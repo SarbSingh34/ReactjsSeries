de3205cb1fce0c23a9a8044af21e6d3f3c179fe0
SARB <mrsinghbusiness05@gmail.com>
2022-09-09 20:37:37 +0530

    state changed in class Comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import ClassComp from './Components/ClassComp';
 
 
 function App() {
   return (
     <div className="App">
-      vevdc
+      veJNKM
+      <ClassComp />
     </div>
   );
 }
--- src/Components/ClassComp.js
+++ src/Components/ClassComp.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
+// Using state 
+
  class ClassComp extends Component 
  {
+    constructor()
+    {
+        super()
+        this.state = {
+            message : 'Welcome visitor'
+        }
+    }
+    handledata()
+    {
+        this.setState({
+            message : 'this is changed'
+        })
+    }
+
   render() {
     return (
-      <div>ClassComp</div>
+      <div>
+         Hello  {this.state.message}
+     <button onClick={() => {this.handledata()}}> Change  </button>     
+     </div>
     )
   }
 }
